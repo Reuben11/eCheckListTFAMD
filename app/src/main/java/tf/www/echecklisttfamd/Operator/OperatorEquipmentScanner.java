9c7099903fcf606a02db900525523f6d66deac33
cvan chow <avira7@gmail.com>
2019-04-05 16:39:15 +0800

    6th Commit - Added equipment barcode scan fragment. (Some Issues event not trigger)

---
+++ app/src/main/java/tf/www/echecklisttfamd/Operator/OperatorEquipmentScanner.java
@@ -0,0 +1,73 @@
+package tf.www.echecklisttfamd.Operator;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import tf.www.echecklisttfamd.LoginActivity;
+import tf.www.echecklisttfamd.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class OperatorEquipmentScanner extends Fragment {
+    private EditText eBarcode;
+    private TextView tvBarcodeLabel;
+    View view;
+
+    TextWatcher textWatcher = new TextWatcher() {
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            Snackbar snackbar = Snackbar.make(tvBarcodeLabel, eBarcode.toString(), Snackbar.LENGTH_LONG);
+            snackbar.show();
+        }
+    };
+
+    public static OperatorEquipmentScanner newInstance() {
+        // Required empty public constructor
+        OperatorEquipmentScanner fragment = new OperatorEquipmentScanner();
+        return fragment;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_operator_equipment_scanner, container, false);
+        eBarcode = view.findViewById(R.id.equipmentbarcode);
+        tvBarcodeLabel = view.findViewById(R.id.barcodelable);
+
+        eBarcode.setShowSoftInputOnFocus(false);
+        eBarcode.addTextChangedListener(textWatcher);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+
+
+    }
+
+}
